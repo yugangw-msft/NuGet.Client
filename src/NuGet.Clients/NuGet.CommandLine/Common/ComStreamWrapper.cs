ffb48a8cc303f207ab74e72175ea61783bdb36d1
Meni Zalzman <meniz@microsoft.com>
2015-11-17 11:44:35 -0800

    Merging the PDB missing sources PR to nuget client branch with minor chages

---
+++ src/NuGet.Clients/NuGet.CommandLine/Common/ComStreamWrapper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using STATSTG = System.Runtime.InteropServices.ComTypes.STATSTG;
+
+namespace NuGet.Common
+{
+    /// <summary>
+    /// Wrap a Stream so it's usable where we need an IStream
+    /// </summary>
+    internal sealed class ComStreamWrapper : IStream, IDisposable
+    {
+        Stream _stream;        
+        private bool _disposed = false;
+
+        /// <summary>
+        /// Create a new adapter around the given stream.
+        /// </summary>
+        /// <param name="wrappedStream">The stream to wrap.</param>
+        public ComStreamWrapper(Stream wrappedStream)
+        {
+            _stream = wrappedStream;
+        }
+
+        ~ComStreamWrapper()
+        {
+            Dispose(false);
+        }
+
+        public void Clone(out IStream ppstm)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Commit(int grfCommitFlags)
+        {
+        }
+
+        public void LockRegion(long libOffset, long cb, int dwLockType)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Revert()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void UnlockRegion(long libOffset, long cb, int dwLockType)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void CopyTo(IStream pstm, long cb, IntPtr pcbRead, IntPtr pcbWritten)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Read(byte[] pv, int cb, IntPtr pcbRead)
+        {
+            var count = _stream.Read(pv, 0, cb);
+            if (pcbRead != IntPtr.Zero)
+            {
+                Marshal.WriteInt32(pcbRead, count);
+            }
+        }
+
+        public void Seek(long dlibMove, int dwOrigin, IntPtr plibNewPosition)
+        {
+            var origin = (SeekOrigin)dwOrigin;
+            var pos = _stream.Seek(dlibMove, origin);
+            if (plibNewPosition != IntPtr.Zero)
+            {
+                Marshal.WriteInt64(plibNewPosition, pos);
+            }
+        }
+
+        public void SetSize(long libNewSize)
+        {
+            _stream.SetLength(libNewSize);
+        }
+
+        public void Stat(out STATSTG pstatstg, int grfStatFlag)
+        {
+            pstatstg = new STATSTG
+            {
+                type = 2,
+                cbSize = _stream.Length,
+                grfMode = 0
+            };
+
+            if (_stream.CanRead && _stream.CanWrite)
+            {
+                pstatstg.grfMode |= 2;
+            }
+            else if (_stream.CanWrite && !_stream.CanRead)
+            {
+                pstatstg.grfMode |= 1;
+            }
+        }
+
+        public void Write(byte[] pv, int cb, IntPtr pcbWritten)
+        {
+            _stream.Write(pv, 0, cb);
+            if (pcbWritten != IntPtr.Zero)
+            {
+                Marshal.WriteInt32(pcbWritten, cb);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Interlocked.Exchange(ref _stream, null)?.Close();                
+                _disposed = true;
+            }
+        }
+    }
+}
---
+++ src/NuGet.Clients/NuGet.CommandLine/Common/PdbReader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security;
+using System.Threading;
+
+using Microsoft.DiaSymReader;
+
+namespace NuGet.Common
+{
+    internal static class PdbReader
+    {
+        public static IEnumerable<string> GetSourceFileNames(IPackageFile pdbFile)
+        {
+            using (var stream = new ComStreamWrapper(pdbFile.GetStream()))
+            {
+                var reader = CreateNativeSymReader(stream);
+
+                return reader.GetDocuments()
+                    .Select(doc => doc.GetName())
+                    .Where(IsValidSourceFileName);
+            }
+        }
+
+        private static ISymUnmanagedReader3 CreateNativeSymReader(IStream pdbStream)
+        {
+            object symReader = null;
+            var guid = default(Guid);
+
+            if (IntPtr.Size == 4)
+            {
+                NativeMethods.CreateSymReader32(ref guid, out symReader);
+            }
+            else
+            {
+                NativeMethods.CreateSymReader64(ref guid, out symReader);
+            }
+
+            var reader = (ISymUnmanagedReader3)symReader;
+            var hr = reader.Initialize(new DummyMetadataImport(), null, null, pdbStream);
+            Marshal.ThrowExceptionForHR(hr);
+            return reader;
+        }
+
+        private static bool IsValidSourceFileName(string sourceFileName)
+        {
+            return !string.IsNullOrEmpty(sourceFileName) && !IsTemporaryCompilerFile(sourceFileName);
+        }
+
+        private static bool IsTemporaryCompilerFile(string sourceFileName)
+        {
+            //the VB compiler will include temporary files in its pdb files.
+            //the source file name will be similar to 17d14f5c-a337-4978-8281-53493378c1071.vb.
+            return sourceFileName.EndsWith("17d14f5c-a337-4978-8281-53493378c1071.vb", StringComparison.OrdinalIgnoreCase);
+        }
+        
+        private class DummyMetadataImport : IMetadataImport { }
+
+        [SuppressUnmanagedCodeSecurity]
+        private static class NativeMethods
+        {
+            [DefaultDllImportSearchPaths(DllImportSearchPath.AssemblyDirectory)]
+            [DllImport("Microsoft.DiaSymReader.Native.x86.dll", EntryPoint = "CreateSymReader")]
+            internal extern static void CreateSymReader32(ref Guid id, [MarshalAs(UnmanagedType.IUnknown)]out object symReader);
+
+            [DefaultDllImportSearchPaths(DllImportSearchPath.AssemblyDirectory)]
+            [DllImport("Microsoft.DiaSymReader.Native.amd64.dll", EntryPoint = "CreateSymReader")]
+            internal extern static void CreateSymReader64(ref Guid id, [MarshalAs(UnmanagedType.IUnknown)]out object symReader);
+        }        
+    }
+
+    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "need to validate that's ok to suppress.")]
+    [ComImport, Guid("7DAC8207-D3AE-4c75-9B67-92801A497D44"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), TypeIdentifier]
+    internal interface IMetadataImport { }
+}
